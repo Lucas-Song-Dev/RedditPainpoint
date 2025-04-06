8d2e411c3aa9a013e86e169e0fc3fccc0ec4963e
Lucas Song <aosdjs@students.cs.ubc.ca>
2025-04-05 19:55:08 -0700

    add readme for server set up to main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Post from "./api/Post";
+import Scrape from "./api/Scrape";
+import Analysis from "./api/GPTAnalysis";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,6 +20,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Post />
+      <Scrape />
+      <Analysis />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +35,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
