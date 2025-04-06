32a038a6c744780d805dee3d67cced1d7b972006
Lucas Song <aosdjs@students.cs.ubc.ca>
2025-04-05 19:32:55 -0700

    server code up and running

--- routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-# Create blueprint for main routes
-main_bp = Blueprint('main', __name__)
-
-@main_bp.route('/')
-def index():
-    """Render the admin dashboard page"""
-    return render_template('index.html')
