3374ac1dd88a7e473b9d42ac1467f6ec947d76b7
Lucas Song <aosdjs@students.cs.ubc.ca>
2025-04-14 14:17:05 -0700

    notification pop up

---
+++ client/src/context/NotificationContext.jsx
@@ -0,0 +1,32 @@
+// src/context/NotificationContext.jsx
+import { createContext, useState, useContext } from "react";
+
+const NotificationContext = createContext();
+
+export const NotificationProvider = ({ children }) => {
+  const [notification, setNotification] = useState(null);
+
+  const showNotification = (message, type = "info", duration = 5000) => {
+    setNotification({ message, type, id: Date.now() });
+
+    if (duration) {
+      setTimeout(() => {
+        setNotification(null);
+      }, duration);
+    }
+  };
+
+  const hideNotification = () => {
+    setNotification(null);
+  };
+
+  return (
+    <NotificationContext.Provider
+      value={{ notification, showNotification, hideNotification }}
+    >
+      {children}
+    </NotificationContext.Provider>
+  );
+};
+
+export const useNotification = () => useContext(NotificationContext);
